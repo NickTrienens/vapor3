284f22c256ffb4ed04d1b15761887c6ca9ecb91b
Nicholas Trienens <nick@fuzzproductions.com>
2018-05-21 08:02:19 -0600

    [split vapor protocols into a seperate extension file allowing for the client to use the Todo model]

--- Sources/App/Models/Todo+Database.swift
+++ Sources/App/Models/Todo+Database.swift
@@ -4,5 +4,20 @@
 //
 //  Created by Nicholas Trienens on 5/15/18.
 //
-
 import Foundation
+import FluentSQLite
+
+import Vapor
+
+
+extension Todo: SQLiteModel {}
+
+
+/// Allows `Todo` to be used as a dynamic migration.
+extension Todo: Migration { }
+
+/// Allows `Todo` to be encoded to and decoded from HTTP messages.
+extension Todo: Content { }
+
+/// Allows `Todo` to be used as a dynamic parameter in route definitions.
+extension Todo: Parameter { }
